eb1e219550dc48a775590fd7364587724352c7b0
thaddeus-tech <mmoffat687@gmail.com>
2025-03-20 16:00:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "l Love Jesus Bot",
+  botOwner: 'Moffat Thaddeus', // Your name
+  ownerNumber: '+254740075968', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
